eb585600455cb25fa719f17735b2b36792fb823f
旧事 <545049148@qq.com>
2026-04-18 23:46:54 +0800

    chore: 版本更新至 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.mcard"
         minSdk = 34
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
